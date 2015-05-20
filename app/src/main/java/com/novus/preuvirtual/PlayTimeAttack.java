c33e7ba61f6c5db121b31c5c0da35790048f9afa
Martín González <marto.gtk@gmail.com>
2015-05-20 16:25:10 -0300

    Agregado contador a PlayTimeAttack

--- app/src/main/java/com/novus/preuvirtual/PlayTimeAttack.java
+++ app/src/main/java/com/novus/preuvirtual/PlayTimeAttack.java
@@ -1,15 +1,20 @@
 package com.novus.preuvirtual;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import static java.lang.Math.floor;
 
 
 public class PlayTimeAttack extends ActionBarActivity {
 
     TextView textTiempo;
+    CountDownTimer backCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,7 @@ public class PlayTimeAttack extends ActionBarActivity {
 
         textTiempo = (TextView)findViewById(R.id.textTiempo);
         Bundle bundle = getIntent().getExtras();
-        textTiempo.setText(bundle.getString("varTiempo"));
-
+        setTimer(Integer.parseInt(bundle.getString("varTiempo")));
     }
 
     @Override
@@ -27,6 +31,13 @@ public class PlayTimeAttack extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_play_time_attack, menu);
         return true;
+    }
+
+    @Override
+    public void onStop(){
+        backCount.cancel();
+        backCount = null;
+        super.onStop();
     }
 
     @Override
@@ -38,4 +49,32 @@ public class PlayTimeAttack extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void setTimer(int minutes) {
+
+        backCount = new CountDownTimer(minutes * 1000 * 60, 1000) {
+            TextView timer = (TextView) findViewById(R.id.textTiempo);
+
+            public void onTick(long remaining) {
+                Log.d("Tiempo", String.valueOf(remaining));
+                long remainingMinutes = (long) floor((remaining / 1000) / 60);
+                long remainingSeconds = (remaining / 1000) % 60;
+                if (remainingSeconds < 10 && remainingMinutes < 10) {
+                    timer.setText("0" + remainingMinutes + ":0" + remainingSeconds);
+                } else if (remainingSeconds > 10 && remainingMinutes < 10) {
+                    timer.setText("0" + remainingMinutes + ":" + remainingSeconds);
+                } else if (remainingSeconds < 10 && remainingMinutes > 10) {
+                    timer.setText("" + remainingMinutes + ":0" + remainingSeconds);
+                } else {
+                    timer.setText("" + remainingMinutes + ":" + remainingSeconds);
+                }
+            }
+
+            public void onFinish() {
+                timer.setText("00:00");
+            }
+
+        }.start();
+    }
+
 }
